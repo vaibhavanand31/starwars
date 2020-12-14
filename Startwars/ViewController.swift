6b9c3c6f280d72112d7358979d201868550ba2ee
Vaibhav <vaibhavanand1591@gmail.com>
2020-12-14 17:48:38 -0500

    Initial Commit

---
+++ Startwars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Startwars
+//
+//  Created by Vaibhav Anand on 2020-12-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
